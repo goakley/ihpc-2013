608dcead98d4a1f83e62b0b2cae28ea9d43091d8
Glen Oakley <goakley123@gmail.com>
2013-03-13 13:46:08 -0400

    Added details to P2

---
+++ p2/p2.c
@@ -0,0 +1,50 @@
+/******************************************************************************
+The Green Lantern Corps is the largest intergalactic peace-keeping force. They patrol the farthest reaches of the Universe at the behest of the Guardians(MASTER NODES), a race of immortals residing on the planet Oa. The Green Lantern Corp's operation is divided as pairs amongst the 3600 'sectors' (NODES) of the universe, over 7200 members (known commonly as Green Lanterns)(Processors) are estimated to be serving within the Corps. Each Green Lantern is given a power ring, a weapon granting the use of incredible abilities that are directed by the wearer's own willpower.
+
+A sudden attack by a powerful force brought in the 'Blackest Night' where both the Corps and the Guardians have suffered huge casualties. The Guardians' failure to take appropriate action during the Crisis has led to the survival of the last remaining guardian YOU - Sodam Yat. Left to their own devices, the Corps underwent a major reorganization and now you have to lead them. A team of Green Lanterns brought the new recruit Hal Jordan from Earth. You, Sodam Yat see a great potential in this green lantern, maybe one day he might save the Universe. So you design special tasks to train him to become a guardian one day.
+
+These task are specially designed so that he couldn't complete it alone. He need to show creativity, teamwork and leadership.
+
+His first task :
+To find the cause of this crisis from the traces of matter found after the war by reading and comparing with the data the guardians collected in the Hall of Knowledge located in planet Zamaron.
+
+Input:
+
+There will be T test cases (<=100). Each test case will be as follows:
+The first line contains a text of size N(<=60000 characters). The next line contains an integer Q(<=500), the number of query strings to follow. Then Q lines follow, each one containing a query string of maximum size M (<=100).
+
+Output:
+T lines, one for each test case.
+For a single test case, print Q(number of query strings in that particular test case) space separated integers, where the i'th integer is the number of times i'th input string occur in the text.
+
+Maximum number of lantenrs(POCESSES) Hal can use including your MASTER NODE skills is 10
+
+Sample Input:
+3
+aaaaa
+2
+a
+aa
+ababababababa
+3
+ab
+aba
+abaa
+abcdabcdabcdabcdabcd
+5
+aba
+dabc
+abcd
+ab
+abcdabcdabcdabcdabcd
+
+
+Sample Output:
+5 4
+6 6 0
+0 4 5 5 1
+******************************************************************************/
+
+
+// I cannot even begin to understand this question based on the description 
+// and the sample I/O.
