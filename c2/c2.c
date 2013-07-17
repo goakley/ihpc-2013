1989866cd910606160c0811a88ddb167067a89fd
Glen Oakley <goakley123@gmail.com>
2013-07-17 10:28:15 -0700

    Added skeleton for C2

---
+++ c2/c2.c
@@ -0,0 +1,25 @@
+/******************************************************************************
+Hal needs to form his team. He chooses green lanters whose enrolement number and the sector to which they belong satisfy certain property - Prime Frequency.
+
+Input:
+The first line consists of an integer T(<=1000), the number of test cases. Then T lines follow.
+Each test case consists of two integers n(sector number) and k(green lanter enrolement no.). n (<=10^100) and given a number k (k<=1000000).
+
+Output:
+For each test case. Print the number of primes below k that divides n and then print them in increasing order in the same line, separating consecutive primes by a space.
+
+Sample Input:
+2
+10 3
+20 6
+
+Sample Output:
+1 2
+2 2 5
+
+Your code must be designed to run on 20 processes using MPI.
+
+C2 Scoring : 500(correct submission) + 100*[speed factor]
+
+Time Limit:10 seconds 
+******************************************************************************/
